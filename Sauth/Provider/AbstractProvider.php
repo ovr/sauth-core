5808a55e5373a3e38ba7e468f521cbd756789b20
nixa <4dmitr@gmail.com>
2012-06-30 22:46:59 +0300

    two spaces replaces with four spaces

--- Sauth/Provider/AbstractProvider.php
+++ Sauth/Provider/AbstractProvider.php
@@ -5,45 +5,53 @@ namespace Sauth\Provider;
 abstract class AbstractProvider
 {
 
-  protected $_configuration = array();
+    /**
+     * Configuration array
+     * @var array
+     */
+    protected $_configuration = array();
 
 
-  public function __construct(array $configuration = array())
-  {
-    $this->setConfiguration($configuration);
-  }
-
-  /**
-   * Sets configuration $value by $key
-   * @param string $key
-   * @param mixed $value
-   */
-  public function addConfiguration($key, $value)
-  {
-    $this->_configuration[$key] = $value;
-  }
-
-  /**
-   * Sets configuration array. Replaces previous one
-   * Configuration will be validated before request part
-   * @param array $configuration
-   */
-  public function setConfiguration($configuration)
-  {
-    $this->_configuration = $configuration;
-  }
-
-  /**
-   * Returns whole configuration array or just a spicified element
-   * @param mixed $key
-   * @return mixed
-   */
-  public function getConfiguration($key = null)
-  {
-    if ($key) {
-      return isset($this->_configuration[$key]) ? $this->_configuration[$key] : false;
-    } else {
-      return $this->_configuration;
+    /**
+     * Provider object constructor
+     * @param array $configuration
+     */
+    public function __construct(array $configuration = array())
+    {
+        $this->setConfiguration($configuration);
     }
-  }
+
+    /**
+     * Sets configuration $value by $key
+     * @param string $key
+     * @param mixed $value
+     */
+    public function addConfiguration($key, $value)
+    {
+        $this->_configuration[$key] = $value;
+    }
+
+    /**
+     * Sets configuration array. Replaces previous one
+     * Configuration will be validated before request part
+     * @param array $configuration
+     */
+    public function setConfiguration($configuration)
+    {
+        $this->_configuration = $configuration;
+    }
+
+    /**
+     * Returns whole configuration array or just a specified element
+     * @param mixed $key
+     * @return mixed
+     */
+    public function getConfiguration($key = null)
+    {
+        if ($key) {
+            return isset($this->_configuration[$key]) ? $this->_configuration[$key] : false;
+        } else {
+            return $this->_configuration;
+        }
+    }
 }
